6c243dd721731169be21c7108117b75e76f98dcc
rahul1224044 <rahuljajoria2412@gmail.com>
2025-09-01 17:58:14 +0530

    all done in frontend

--- chat-app-frontend/src/pages/login.jsx
+++ chat-app-frontend/src/pages/login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
   e.preventDefault();
   try {
-    const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/user/login`, form, { withCredentials: true });
+    const res = await axios.post(`${import.meta.env.VITE_API_URL}api/user/login`, form, { withCredentials: true });
     console.log(res);
     sessionStorage.setItem("token", res.data.token);
     sessionStorage.setItem("user", JSON.stringify(res.data.user));
