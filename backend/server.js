92200133537911bb731d3a321dc9698d98a6aec9
rahul1224044 <rahuljajoria2412@gmail.com>
2025-09-03 19:22:54 +0530

    all done

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const httpServer = createServer(app);
 const allowedOrigins = [
-  "https://talkrandoms.netlify.app",
+  "https://arconnect.netlify.app",
   "https://randomfriendstalking.onrender.com",
   "http://localhost:5173",
   "http://localhost:3000"
